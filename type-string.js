f3830a419c6196a44c3ad3ea3e035f2d436016db
Vitor Miranda <uelber.vitor@gmail.com>
2023-10-23 20:56:38 -0300

    curso java script

---
+++ type-string.js
@@ -0,0 +1,27 @@
+const palavra = 'vitor';
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+let cadastroAtivado = true;
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2);
